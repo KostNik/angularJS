e63949c0148f9cd1f0475e391e05cd28ab2e3271
Kostiuk_N <kost.nik88@gmail.com>
2018-08-26 23:16:29 +0300

    bc

--- 002_TemplateDerectives/Samples/04_Partial_views/02_ng-include/app.js
+++ 002_TemplateDerectives/Samples/04_Partial_views/02_ng-include/app.js
@@ -2,21 +2,28 @@
 
 app.controller("SampleAppCtrl", function ($scope) {
 
-    $scope.items = [{ name: "Item 1", value: "Value 1" },
-        { name: "Item 2", value: "Value 2" },
-        { name: "Item 3", value: "Value 3" }];
+    $scope.items = [{name: "Item 1", value: "Value 1"},
+        {name: "Item 2", value: "Value 2"},
+        {name: "Item 3", value: "Value 3"}];
 
     $scope.tableView = "table.html";
 
     $scope.listView = "list.html";
 
+    $scope.smthView = "smth.html";
+
     $scope.url = $scope.tableView;
 
-    $scope.showList = function () {
+        $scope.showList = function () {
         $scope.url = $scope.listView;
-    }
+    };
 
     $scope.showTable = function () {
         $scope.url = $scope.tableView;
+    };
+
+    $scope.showSmth = function () {
+        $scope.url = $scope.smthView;
     }
+
 });
